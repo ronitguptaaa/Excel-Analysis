f460fd7749b81e3a9dcbe84b8e62107c302ccd44
ronitguptaaa <ronitgupta190@gmail.com>
2024-06-23 13:55:23 +0530

    done

--- Project-1_DS/EmployeeSurveyScript.sql
+++ Project-1_DS/EmployeeSurveyScript.sql
@@ -1,7 +1,10 @@
+-- Creating a CTE to clean and standardize EmployeeReview data
+
+CREATE TABLE CompleteEmployeeSurveyDetails AS (
 WITH EmployeeReview AS (
     SELECT DISTINCT
         `Respondent ID` AS RespondentID,
-        STR_TO_DATE (`Start Date`, '%c/%e/%Y') AS StartDate,
+        STR_TO_DATE(`Start Date`, '%c/%e/%Y %H:%i') AS StartDate,
         Race,
         CASE
             WHEN Race = 'Asian' THEN 1
@@ -16,20 +19,17 @@ WITH EmployeeReview AS (
             WHEN Race = 'Declined to Answer' THEN 10
         END AS RaceID,
         CASE
-            WHEN Gender = 'F' THEN 'Female'
-            WHEN Gender = 'Femail' THEN 'Female'
-            WHEN Gender = 'Females' THEN 'Female'
-            WHEN Gender = 'Femalr' THEN 'Female'
-            WHEN Gender = 'M' THEN 'Male'
-            WHEN Gender = 'male' THEN 'Male'
-            WHEN Gender = 'Man' THEN 'Male'
-            WHEN Gender = 'Technically female' THEN 'Female'
+            WHEN Gender IN ('F', 'Femail', 'Females', 'Femalr', 'Technically female') THEN 'Female'
+            WHEN Gender IN ('M', 'male', 'Man') THEN 'Male'
             ELSE Gender
         END AS Gender,
         CASE
             WHEN Age LIKE '%-%' THEN
                 (CAST(SUBSTRING_INDEX(Age, '-', 1) AS DECIMAL) + CAST(SUBSTRING_INDEX(Age, '-', -1) AS DECIMAL)) / 2
-            ELSE CAST(Age AS DECIMAL)
+            WHEN Age LIKE '%+%' THEN
+                CAST(SUBSTRING_INDEX(Age, '+', 1) AS DECIMAL)
+            ELSE
+                CAST(Age AS DECIMAL)
         END AS Age,
         EducationLevel,
         CASE 
@@ -60,35 +60,79 @@ WITH EmployeeReview AS (
             WHEN ExperienceLevel = 'Declined to Answer' THEN 5
         END AS ExperienceLevelID,
         JobTitle,
-        ROW_NUMBER() OVER(ORDER BY JobTitle) AS JobTitleID,
         SeniorityLevel,
-        CASE
-            WHEN Promotions = 'Prefer not to answer' THEN 'NA'
-            ELSE Promotions
-        END AS Promotions,
+        Promotions,
+        CASE 
+            WHEN Promotions = 'In the last 1 to 5 years' THEN 1
+            WHEN Promotions = 'More than 5 years' THEN 2
+            WHEN Promotions = 'Multiple Promotions' THEN 3
+            WHEN Promotions = 'Never been promoted' THEN 4
+            WHEN Promotions = 'Prefer not to answer' THEN 5
+            WHEN Promotions = 'Within 12 Months' THEN 6
+        END AS PromotionsID,
         Industry,
         BusinessType,
-        CompanySize,
-        CASE
-            WHEN JobPerformance = 'Declined to Answer' THEN 'NA'
-            ELSE JobPerformance
-        END AS JobPerformance,
-        CASE
-            WHEN IsCompensation = 'Declined to Answer' THEN 'NA'
-            ELSE IsCompensation
-        END AS IsCompensation,
-        CASE
-            WHEN IsGrowth = 'Declined to Answer' THEN 'NA'
-            ELSE IsGrowth
-        END AS IsGrowth,
-        CASE
-            WHEN IsDecisions = 'Declined to Answer' THEN 'NA'
-            ELSE IsDecisions
-        END AS IsDecisions,
-        CASE
-            WHEN IsGrowthOpportunities = 'Declined to Answer' THEN 'NA'
-            ELSE IsGrowthOpportunities
-        END AS IsGrowthOpportunities
+        CASE 
+            WHEN BusinessType = 'For Profit' THEN 1
+            WHEN BusinessType = 'Government' THEN 2
+            WHEN BusinessType = 'Non Profit' THEN 3
+            WHEN BusinessType = 'Unknown' THEN 4
+        END AS BusinessTypeID,
+        JobPerformance,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(JobPerformance))) = 'agree' THEN 8
+END AS JobPerformanceID,
+        IsCompensation,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(IsCompensation))) = 'agree' THEN 8
+END AS IsCompensationID,
+        IsGrowth,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(IsGrowth))) = 'agree' THEN 8
+END AS IsGrowthID,
+        IsDecisions,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(IsDecisions))) = 'agree' THEN 8
+END AS IsDecisionsID,
+        IsGrowthOpportunities,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(IsGrowthOpportunities))) = 'agree' THEN 8
+END AS IsGrowthOpportunitiesID
     FROM
         ExcelAnalysis.EmployeeReview
     WHERE
@@ -105,21 +149,38 @@ WITH EmployeeReview AS (
         AND Promotions IS NOT NULL
         AND Industry IS NOT NULL
         AND BusinessType IS NOT NULL
-        AND CompanySize IS NOT NULL
         AND JobPerformance IS NOT NULL
         AND IsCompensation IS NOT NULL
         AND IsGrowth IS NOT NULL
         AND IsDecisions IS NOT NULL
         AND IsGrowthOpportunities IS NOT NULL
 ),
+-- Creating a CTE to clean and standardize EmployeeSurvey data
 EmployeeSurvey AS (
     SELECT DISTINCT
         RespondentID,
         PostalCode,
         Salary,
         BonusStatus,
-        EmployeeCount AS CompanyEmployeeCount,
-        IsFairness
+        EmployeeCount,
+        CASE 
+            WHEN EmployeeCount = '1,000 to 10,000 Employees' THEN 1
+            WHEN EmployeeCount = '250 to 1,000 Employees' THEN 2
+            WHEN EmployeeCount = '50 to 250 Employees' THEN 3
+            WHEN EmployeeCount = 'Less than 50 Employees' THEN 4
+            WHEN EmployeeCount = 'More than 10,000 Employees' THEN 5
+        END AS CompanyEmployeeCountID,
+        IsFairness,
+        CASE 
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'strongly disagree' THEN 1
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'strongly agree' THEN 2
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'somewhat disagree' THEN 3
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'somewhat agree' THEN 4
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'neither agree nor disagree' THEN 5
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'disagree' THEN 6
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'declined to answer' THEN 7
+    WHEN LTRIM(RTRIM(LOWER(IsFairness))) = 'agree' THEN 8
+END AS IsFairnessID
     FROM
         ExcelAnalysis.EmployeeSurvey
     WHERE
@@ -129,8 +190,23 @@ EmployeeSurvey AS (
         AND BonusStatus IS NOT NULL
         AND EmployeeCount IS NOT NULL
         AND IsFairness IS NOT NULL
+),
+
+-- Creating mappings for various columns using ROW_NUMBER()
+JobTitleMapping AS (
+    SELECT DISTINCT LTRIM(RTRIM(LOWER(JobTitle))) AS JobTitle, ROW_NUMBER() OVER (ORDER BY LTRIM(RTRIM(LOWER(JobTitle)))) AS JobTitleID FROM EmployeeReview
+),
+SeniorityLevelMapping AS (
+    SELECT DISTINCT LTRIM(RTRIM(LOWER(SeniorityLevel))) AS SeniorityLevel, ROW_NUMBER() OVER (ORDER BY LTRIM(RTRIM(LOWER(SeniorityLevel)))) AS SeniorityLevelID FROM EmployeeReview
+),
+IndustryMapping AS (
+    SELECT DISTINCT LTRIM(RTRIM(LOWER(Industry))) AS Industry, ROW_NUMBER() OVER (ORDER BY LTRIM(RTRIM(LOWER(Industry)))) AS IndustryID FROM EmployeeReview
+),
+BonusStatusMapping AS (
+    SELECT DISTINCT LTRIM(RTRIM(LOWER(BonusStatus))) AS BonusStatus, ROW_NUMBER() OVER (ORDER BY LTRIM(RTRIM(LOWER(BonusStatus)))) AS BonusStatusID FROM EmployeeSurvey
 )
 
+-- Final SELECT statement to join the data and mappings
 SELECT
     ER.RespondentID,
     ER.StartDate,
@@ -149,22 +225,38 @@ SELECT
     ER.ExperienceLevel,
     ER.ExperienceLevelID,
     ER.JobTitle,
-    ER.JobTitleID,
+    JTM.JobTitleID,
     ER.SeniorityLevel,
+    SLM.SeniorityLevelID,
     ER.Promotions,
+    ER.PromotionsID,
     ER.Industry,
+    IM.IndustryID,
     ER.BusinessType,
-    ER.CompanySize,
+    ER.BusinessTypeID,
     ER.JobPerformance,
+    ER.JobPerformanceID,
     ER.IsCompensation,
+    ER.IsCompensationID,
     ER.IsGrowth,
+    ER.IsGrowthID,
     ER.IsDecisions,
+    ER.IsDecisionsID,
     ER.IsGrowthOpportunities,
+    ER.IsGrowthOpportunitiesID,
     ES.PostalCode,
     ES.Salary,
     ES.BonusStatus,
-    ES.CompanyEmployeeCount,
-    ES.IsFairness
+    BSM.BonusStatusID,
+    ES.EmployeeCount,
+    ES.CompanyEmployeeCountID,
+    ES.IsFairness,
+    ES.IsFairnessID
 FROM
     EmployeeReview AS ER
-    INNER JOIN EmployeeSurvey AS ES ON ER.RespondentID = ES.RespondentID;
+    INNER JOIN EmployeeSurvey AS ES ON ER.RespondentID = ES.RespondentID
+    INNER JOIN JobTitleMapping AS JTM ON LTRIM(RTRIM(LOWER(ER.JobTitle))) = LTRIM(RTRIM(LOWER(JTM.JobTitle)))
+    INNER JOIN SeniorityLevelMapping AS SLM ON LTRIM(RTRIM(LOWER(ER.SeniorityLevel))) = LTRIM(RTRIM(LOWER(SLM.SeniorityLevel)))
+    INNER JOIN IndustryMapping AS IM ON LTRIM(RTRIM(LOWER(ER.Industry))) = LTRIM(RTRIM(LOWER(IM.Industry)))
+    INNER JOIN BonusStatusMapping AS BSM ON LTRIM(RTRIM(LOWER(ES.BonusStatus))) = LTRIM(RTRIM(LOWER(BSM.BonusStatus)))
+    );
